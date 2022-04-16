d2ae136b951d1f46b5cece9c9aabbafa7cb03485
James Wu <jameswu62@yahoo.co.nz>
2022-04-16 21:09:07 +1200

    refactor

--- my_lib/include/queue.h
+++ my_lib/include/queue.h
@@ -19,18 +19,26 @@ extern "C"
 {
 #endif
 
-    typedef struct queue_typeDef* queue_handle_t;
+    typedef struct queue_typeDef *queue_handle_t;
+
+    typedef struct
+    {
+        void *const ptrData;
+        const size_t len;
+    } queue_obj_t;
 
     queue_handle_t queue_create(void);
-    void queue_destroy(queue_handle_t handle);
-    int queue_getItemCount(queue_handle_t handle);
-    int queue_wait(queue_handle_t handle);
-    void *queue_peek(queue_handle_t handle);
-    bool queue_pushBack(queue_handle_t handle, void *const obj, const size_t len);
-    bool queue_pushFront(queue_handle_t handle, void *const obj, const size_t len);
-    bool queue_pushCopy(queue_handle_t handle, void *const obj, const size_t len);
-    void *queue_pop(queue_handle_t handle);
-    void queue_dequeueAndFreeMemory(queue_handle_t handle, void (*cbProcessData)(void *const obj, const size_t len));
+    void queue_destroy(queue_handle_t const handle);
+    int queue_getItemCount(queue_handle_t const handle);
+    int queue_wait(queue_handle_t const handle);
+    bool queue_pushBack(queue_handle_t const handle, queue_obj_t *const obj);
+    bool queue_pushFront(queue_handle_t const handle, queue_obj_t *const obj);
+    queue_obj_t *queue_peek(queue_handle_t const handle);
+    queue_obj_t *queue_pop(queue_handle_t const handle);
+
+    void queue_helper_popAndFreeMemory(queue_handle_t const handle, void (*const cbProcessData)(queue_obj_t *const obj));
+    bool queue_helper_pushBackCopy(queue_handle_t const handle, queue_obj_t *const obj);
+    bool queue_helper_pushFrontCopy(queue_handle_t const handle, queue_obj_t *const obj);
 
 #ifdef __cplusplus
 }
