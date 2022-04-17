d2b8d93f321f9bb53676a36940c438a99d7c8a0a
James Wu <jameswu62@yahoo.co.nz>
2022-04-18 10:27:36 +1200

    add comment

--- main.cpp
+++ main.cpp
@@ -5,13 +5,16 @@
 
 int main(int argc, char **argv)
 {
-    // dummy code to get rid of unused parameter warning
+    // Dummy code to get rid of unused parameter warning
     (void)argv;
     (void)argc;
 
     std::printf("hello world!\n");
 
-    for(size_t i = 0; i < 1024UL * 1024UL * 512UL; i++)
+    // Test create and destroy. There should be no significant
+    // memory userage after this. If there is significant memory
+    // userage, there is a memory leak.
+    for (size_t i = 0; i < 1024UL * 1024UL * 512UL; i++)
     {
         queue_handle_t handle = queue_create();
         queue_destroy(handle);
