ed581f2ac492e0ef4dc6bd4e98d99b46ed027b85
James Wu <jameswu62@yahoo.co.nz>
2022-04-18 10:41:26 +1200

    add main test

--- main.cpp
+++ main.cpp
@@ -9,16 +9,41 @@ int main(int argc, char **argv)
     (void)argv;
     (void)argc;
 
-    std::printf("hello world!\n");
+    queue_handle_t handle = NULL;
 
     // Test create and destroy. There should be no significant
     // memory userage after this. If there is significant memory
     // userage, there is a memory leak.
+
+    std::printf("Testing queue create and destroy...\n");
+
     for (size_t i = 0; i < 1024UL * 1024UL * 512UL; i++)
     {
-        queue_handle_t handle = queue_create();
+        handle = queue_create();
         queue_destroy(handle);
     }
+
+    // Test push and pop. There should be no significant
+    // memory userage after this. If there is significant memory
+    // userage, there is a memory leak.
+
+    std::printf("Testing queue push and pop...\n");
+
+    handle = queue_create();
+    const int buf_size = 32;
+    char *buffer = (char *)malloc(buf_size);
+    queue_obj_t push_obj = {
+        .ptrData = buffer,
+        .len = buf_size};
+
+    for (size_t i = 0; i < 1024UL * 1024UL * 512UL; i++)
+    {
+        queue_pushBack(handle, &push_obj);
+        queue_obj_t pop_obj = queue_pop(handle);
+    }
+
+    free(buffer);
+    queue_destroy(handle);
 
     return EXIT_SUCCESS;
 }
