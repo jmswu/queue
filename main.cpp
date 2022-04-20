28abb6a93dd6b757255b4e6cf96851857c80cd40
James Wu <jameswu62@yahoo.co.nz>
2022-04-21 09:12:42 +1200

    refactor

--- main.cpp
+++ main.cpp
@@ -3,38 +3,51 @@
 #include <iostream>
 #include "queue.h"
 
+void runTest_queueCreateAndDestroy(void);
+void runTest_queuePushAndPop(void);
+
 int main(int argc, char **argv)
 {
     // Dummy code to get rid of unused parameter warning
     (void)argv;
     (void)argc;
 
+    runTest_queueCreateAndDestroy();
+    runTest_queuePushAndPop();
+
+    return EXIT_SUCCESS;
+}
+
+void runTest_queueCreateAndDestroy(void)
+{
     queue_handle_t handle = NULL;
+    std::printf("Testing queue create and destroy...\n");
 
     // Test create and destroy. There should be no significant
     // memory userage after this. If there is significant memory
     // userage, there is a memory leak.
-
-    std::printf("Testing queue create and destroy...\n");
 
     for (size_t i = 0; i < 1024UL * 1024UL * 512UL; i++)
     {
         handle = queue_create();
         queue_destroy(handle);
     }
+}
 
-    // Test push and pop. There should be no significant
-    // memory userage after this. If there is significant memory
-    // userage, there is a memory leak.
-
+void runTest_queuePushAndPop(void)
+{
     std::printf("Testing queue push and pop...\n");
 
-    handle = queue_create();
+    queue_handle_t handle = queue_create();
     const int buf_size = 32;
     char *buffer = (char *)malloc(buf_size);
     queue_obj_t push_obj = {
         .ptrData = buffer,
         .len = buf_size};
+
+    // Test push and pop. There should be no significant
+    // memory userage after this. If there is significant memory
+    // userage, there is a memory leak.
 
     for (size_t i = 0; i < 1024UL * 1024UL * 512UL; i++)
     {
@@ -44,6 +57,4 @@ int main(int argc, char **argv)
 
     free(buffer);
     queue_destroy(handle);
-
-    return EXIT_SUCCESS;
 }
