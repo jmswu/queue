10d9cc44fdc10e6d5edce34d1a1567b27a70e9a8
James Wu <jameswu62@yahoo.co.nz>
2022-04-21 09:06:26 +1200

    add test

--- unit_test/queue_test.cpp
+++ unit_test/queue_test.cpp
@@ -281,5 +281,19 @@ namespace
         ASSERT_EQ(1, queue_getItemCount(handle));
     }
 
-    
+    TEST_F(QueueTest, itemCountShouldBeTwo)
+    {
+        // create obj
+        const int BUFF_SIZE_1 = 32;
+        const int BUFF_SIZE_2 = 64;
+        TestQueueObj testObj_1{"1.Hello world one", BUFF_SIZE_1};
+        TestQueueObj testObj_2{"2.Hello world two", BUFF_SIZE_2};
+        queue_obj_t push_obj_1 = testObj_1.getTestQueueObj();
+        queue_obj_t push_obj_2 = testObj_2.getTestQueueObj();
+
+        queue_pushBack(handle, &push_obj_1);
+        queue_pushBack(handle, &push_obj_2);
+
+        ASSERT_EQ(2, queue_getItemCount(handle));
+    }
 }
